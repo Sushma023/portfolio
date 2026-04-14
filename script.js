66ed41931d22a13f999575d2c7972157d1a5de06
Sushma023 <sushma.m8104@gmail.com>
2026-04-14 20:33:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        if(this.getAttribute('href').startsWith("#")) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
